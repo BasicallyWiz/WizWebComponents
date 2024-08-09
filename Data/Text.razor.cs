a39e1a397cf83dfaa63a72429498fcfd1540a878
BasicallyWiz <48531736+BasicallyWiz@users.noreply.github.com>
2024-08-08 22:06:54 -0700

    Add project files.

---
+++ Data/Text.razor.cs
@@ -0,0 +1,46 @@
+﻿/*
+  Hi!
+  I made this component to make it easier to have consistent and quick styling across the web app,
+  but honestly it might just be better to not use these, and opt for typical html while using set
+  style classes. But hey, maybe this is more convenient; I don't know.
+*/
+
+using Microsoft.AspNetCore.Components;
+
+namespace WizWebComponents.Data
+{
+  partial class Text
+  {
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+    [Parameter] public string? Size { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object>? SplatValues { get; set; }
+    string TextSizeClass = "wiz-font-body";
+
+    protected override Task OnParametersSetAsync()
+    {
+      switch (Size)
+      {
+        case "Body":
+          TextSizeClass = "wiz-font-body";
+          break;
+        case "Subtitle":
+          TextSizeClass = "wiz-font-subtitle";
+          break;
+        case "Title":
+          TextSizeClass = "wiz-font-title";
+          break;
+        case "Title-L":
+          TextSizeClass = "wiz-font-title-l";
+          break;
+        case "Display":
+          TextSizeClass = "wiz-font-display";
+          break;
+        default:
+          TextSizeClass = "wiz-font-body";
+          break;
+      }
+
+      return base.OnParametersSetAsync();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Wiz' Web Components
+A collection of Blazor components I've made so I don't have to keep re-writing shit I've made before.
+
+This project can also be used as a good example of component design, and programming practices, as I'm pretty confident with those.
+This also shows code-behind and how to use that pretty well.
+
+## What's with the empty wwwroot?
+I write this while working on another project, and usually when I add styles and assets to the folder, they usually just don't work at all. (I believe the story might be different after solution publish)
+
+## How To Use
+### I want the code (Preferred)
+If you'd like to have the ability to write code into this package, you can simply add this .csproj to any dependent packages in your project references.
+### I just want the components
+You can publish this project to a .dll, and include that, I haven't tried it, but It'll probably work.
